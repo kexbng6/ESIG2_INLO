5054602c43262f14c27c5408e3fa0ecac1545843
Kevin Bonga <elv-kevin-eric.bng@eduge.ch>
2020-12-14 22:22:57 +0100

    login client fonctionnel + redirection page panier, page catalogue

--- testFonctionelSelenium/main.py
+++ testFonctionelSelenium/main.py
@@ -5,24 +5,63 @@
 import time
 from selenium import webdriver
 
-# driver = webdriver.Chrome('/path/to/chromedriver')  # Optional argument, if not specified will search path.
-# driver.get('http://www.google.com/');
-# time.sleep(5) # Let the user actually see something!
-# search_box = driver.find_element_by_name('q')
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-# time.sleep(5) # Let the user actually see something!
-# driver.quit()
 
+
+# https://stackoverflow.com/questions/30543698/how-to-create-a-single-webdriver-instance-in-python
 
 
 def runBrowser():
-    driver = webdriver.Chrome('chromedriver.exe')
-    driver.get('https://sgpc2.pythonanywhere.com/')
-    time.sleep(25) # Let the user actually see something!
-    return driver
+    driver = webdriver.Chrome('../chromedriver')
+    #driver = webdriver.Chrome('chromedriver.exe')
+    driver.maximize_window()
+    driver.get('https://sgpc2.pythonanywhere.com/compte/login/')
+    driver.implicitly_wait(6)
+
+#loginClient
+    driver.find_element_by_id("id_username").send_keys("lkrolik1@usda.gov")
+    driver.find_element_by_id("id_password").send_keys("Motdepasse1234")
+    time.sleep(3)
+    driver.find_element_by_id("submit").click()
+
+    time.sleep(6)
+
+#testSetUp
+    driver.find_element_by_link_text("Mon panier").click()
+    # if driver.find_element_by_tag_name("h1"):
+    #     driver.find_element_by_id("retourBoutique").click()
+    # else:
+    #     driver.find_element_by_link_text("Vider le panier").click()
+    #     driver.implicitly_wait(6)
+    #     driver.find_element_by_link_text("Catalogue").click()
+
+#testAjoutProduit
+    #driver.find_element_by_link_text("acheter").click()
+    driver.implicitly_wait(5)
+    driver.close()
+    #driver.find_element_by_link_text("Ajouter au panier").click()
+
+    time.sleep(7)
+    #driver.find_element_by_name("se déconnecter").click()
+
+#testAugmenterQuantite
+
+#testDiminuerQuantite
+
+#testDiminuerUneUnite
+
+#testPrixTotalLigne
+
+#testRetraitArticle
+
+#-testViderPanier
+
+def setUp():
+    runBrowser()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    runBrowser()
+    setUp()
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
